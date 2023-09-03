3dc519f9f07f0f0c590538de54491dce2bf66af1
avivasyuta <avivasyuta@avito.ru>
2023-09-03 19:03:28 +0400

    3 sum with hash set

---
+++ arrays/three_sum/go/solution_hash_set.go
@@ -0,0 +1,38 @@
+package main
+
+import "sort"
+
+func threeSumHashSet(nums []int) [][]int {
+	result := make([][]int, 0)
+
+	// Предварительно сортируем слайс по возрастанию
+	sort.Ints(nums)
+
+	for i, firstNum := range nums {
+		// Не проверяем текущее число, если оно такое же, как и предыдущее, потому для него мы получим такой же результат.
+		if i != 0 && nums[i-1] == firstNum {
+			continue
+		}
+
+		indexMap := make(map[int]bool)
+
+		for j := i + 1; j < len(nums); j++ {
+			secondNum := nums[j]
+
+			// Высчитываем третье искомое число
+			thirdNum := 0 - firstNum - secondNum
+
+			_, ok := indexMap[thirdNum]
+			if ok {
+				result = append(result, []int{firstNum, secondNum, thirdNum})
+				for j+1 < len(nums) && nums[j] == nums[j+1] {
+					j++
+				}
+			}
+
+			indexMap[secondNum] = true
+		}
+	}
+
+	return result
+}
--- arrays/three_sum/go/solution_two_pointers.go
+++ arrays/three_sum/go/solution_two_pointers.go
@@ -5,15 +5,17 @@ import "sort"
 func threeSumTwoPointers(nums []int) [][]int {
 	result := make([][]int, 0)
 
+	// Предварительно сортируем слайс по возрастанию
 	sort.Ints(nums)
 
 	for i, num := range nums {
-		left := i + 1
-		right := len(nums) - 1
-
+		// Не проверяем текущее число, если оно такое же, как и предыдущее, потому для него мы получим такой же результат.
 		if i != 0 && nums[i-1] == num {
 			continue
 		}
+
+		left := i + 1
+		right := len(nums) - 1
 
 		for left < right {
 			sum := num + nums[left] + nums[right]
