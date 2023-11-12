79a7ae7ffea9b316310289e381d3bf2d9ad7be0b
avivasyuta <avivasyuta@avito.ru>
2023-11-12 23:12:18 +0400

    contains duplicates solution

---
+++ tasks/arrays/contains_duplicates/go/solution_sort.go
@@ -0,0 +1,15 @@
+package contains_duplicates
+
+import "sort"
+
+func containsDuplicateWithSort(nums []int) bool {
+	sort.Ints(nums)
+
+	for i := 0; i < len(nums)-1; i++ {
+		if nums[i] == nums[i+1] {
+			return true
+		}
+	}
+
+	return false
+}
