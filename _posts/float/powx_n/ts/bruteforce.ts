e77fcacb6e7b2125caf79aa1ff8584b58822933c
avivasyuta <avivasyuta@avito.ru>
2023-11-20 09:01:08 +0400

    powx_n ts solution

--- _posts/float/powx_n/ts/bruteforce.ts
+++ _posts/float/powx_n/ts/bruteforce.ts
@@ -1,37 +1,41 @@
-// const powAbsNBruteforce = (x: number, n: number): number => {
-//     if (n === 0) {
-//         return 1
-//     }
-//
-//     if (n === 1) {
-//         return x
-//     }
-//
-//     let res = x
-//
-//     for (n; n > 1; n--) {
-//         res *= x
-//     }
-//
-//     return res
-// }
-//
-// export const myPowBruteforce = (x: number, n: number): number => {
-//     if (x === 1) {
-//         return 1
-//     }
-//
-//     if (x == +-1) {
-//         if (n % 2 === 0) {
-//             return 1
-//         }
-//
-//         return -1
-//     }
-//
-//     if (n < 0) {
-//         return 1 / powAbsNBruteforce(x, -1 * n)
-//     }
-//
-//     return powAbsNBruteforce(x, n)
-// }
+const powAbsNBruteforce = (x: number, n: number): number => {
+    if (n === 0) {
+        return 1
+    }
+
+    if (n === 1) {
+        return x
+    }
+
+    let res = x
+
+    for (n; n > 1; n--) {
+        res *= x
+    }
+
+    return res
+}
+
+export const myPowBruteforce = (x: number, n: number): number => {
+    if (x === 1) {
+        return 1
+    }
+
+    if (x == -1) {
+        if (n % 2 === 0) {
+            return 1
+        }
+
+        return -1
+    }
+
+    let res: number
+
+    if (n < 0) {
+        res = 1 / powAbsNBruteforce(x, -1 * n)
+    } else {
+        res = powAbsNBruteforce(x, n)
+    }
+
+    return parseFloat(res.toFixed(5))
+}
--- _posts/float/powx_n/ts/solution.ts
+++ _posts/float/powx_n/ts/solution.ts
@@ -1,37 +1,39 @@
-// TODO решить задачу на TS
+const powAbsN = (x: number, n: number): number => {
+    if (n === 0) {
+        return 1
+    }
 
-// const powAbsN = (x: number, n: number): number => {
-//     if (n === 0) {
-//         return 1
-//     }
-//
-//     if (n === 1) {
-//         return x
-//     }
-//
-//     if (n % 2 != 0) {
-//         return powAbsN(x * x, n / 2) * x
-//     }
-//
-//     return powAbsN(x * x, n / 2)
-// }
-//
-// export const myPow = (x: number, n: number): number => {
-//     if (x === 1) {
-//         return 1
-//     }
-//
-//     if (x === -1) {
-//         if (n % 2 === 0) {
-//             return 1
-//         }
-//
-//         return -1
-//     }
-//
-//     if (n < 0) {
-//         return 1 / powAbsN(x, -1 * n)
-//     }
-//
-//     return powAbsN(x, n)
-// }
+    if (n === 1) {
+        return x
+    }
+
+    if (n % 2 != 0) {
+        return powAbsN(x * x, Math.floor(n / 2)) * x
+    }
+
+    return powAbsN(x * x, Math.floor(n / 2))
+}
+
+export const myPow = (x: number, n: number): number => {
+    if (x === 1) {
+        return 1
+    }
+
+    if (x === -1) {
+        if (n % 2 === 0) {
+            return 1
+        }
+
+        return -1
+    }
+
+    let res: number
+
+    if (n < 0) {
+        res = 1 / powAbsN(x, -1 * n)
+    } else {
+        res = powAbsN(x, n)
+    }
+
+    return parseFloat(res.toFixed(5))
+}
