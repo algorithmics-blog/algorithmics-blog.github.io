c8d869ac605f544ddbea61d8b00700228bc898f1
avivasyuta <avivasyuta@avito.ru>
2024-06-02 16:07:48 +0800

    maximum_frequency_stack ts solution

--- content/blog/maximum_frequency_stack/ts/solution.spec.ts
+++ content/blog/maximum_frequency_stack/ts/solution.spec.ts
@@ -1,38 +1,31 @@
-import { maxVowels } from "./solution";
+import { FreqStack } from './solution';
 
 type Suit = {
     name: string
-    s: string
-    k: number
-    expected: number
+    input: number[]
+    out: number[]
 }
 
-const suits: Suit[] = [
+const testCases: Suit[] = [
     {
-        name: "abciiidef",
-        s: "abciiidef",
-        k: 3,
-        expected: 3,
+        name: '[5,7,5,7,4,5]',
+        input: [5, 7, 5, 7, 4, 5],
+        out: [5, 7, 5, 4]
     },
-    {
-        name: "aeiou",
-        s: "aeiou",
-        k: 2,
-        expected: 2,
-    },
-    {
-        name: "leetcode",
-        s: "leetcode",
-        k: 3,
-        expected: 2,
-    },
-]
+];
 
-describe('maxVowels', () => {
-    suits.forEach(suit => {
-        test(suit.name, () => {
-            const res = maxVowels(suit.s, suit.k)
-            expect(res).toEqual(suit.expected);
-        })
-    })
-})
+describe('FreqStack', () => {
+    testCases.forEach(c => {
+        test(c.name, () => {
+            const stack = new FreqStack();
+            c.input.forEach(element => stack.push(element));
+
+            const res: number[] = [];
+            for (let i = 0; i < c.out.length; i++) {
+                res.push(stack.pop());
+            }
+
+            expect(res).toEqual(c.out);
+        });
+    });
+});
